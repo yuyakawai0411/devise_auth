66a17384a46eb73ac729fe3c4b0209dd4c821e64
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-11-20 16:20:32 +0900

    ログイン認証機能

---
+++ app/controllers/auth/registrations_controller.rb
@@ -0,0 +1,9 @@
+class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  
+  private
+
+  def sign_up_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
